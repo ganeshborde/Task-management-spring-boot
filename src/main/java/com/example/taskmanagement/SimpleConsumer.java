652ee47dd73faca9088749ec2a86d872c4d40a09
ganeshborde <52167545+ganeshborde@users.noreply.github.com>
2026-03-12 14:08:07 +0530

    change 2nd time for jenkin pull

--- src/main/java/com/example/taskmanagement/SimpleConsumer.java
+++ src/main/java/com/example/taskmanagement/SimpleConsumer.java
@@ -13,8 +13,13 @@ public class SimpleConsumer {
 	public static void main(String[] args) {
 		 Properties props = new Properties();
 	        props.put("bootstrap.servers", "localhost:9092");
+	        
+	        
 	        props.put("group.id", "test-group");
 	        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+	        
+	        
+	        
 	        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
 	        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
