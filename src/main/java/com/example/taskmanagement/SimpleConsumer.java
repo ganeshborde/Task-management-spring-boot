1154dc3155bfa0fd3ec652bf4704def3ca89ce82
ganeshborde <52167545+ganeshborde@users.noreply.github.com>
2026-03-12 13:38:09 +0530

    just tp

--- src/main/java/com/example/taskmanagement/SimpleConsumer.java
+++ src/main/java/com/example/taskmanagement/SimpleConsumer.java
@@ -13,7 +13,7 @@ public class SimpleConsumer {
 	public static void main(String[] args) {
 		 Properties props = new Properties();
 	        props.put("bootstrap.servers", "localhost:9092");
-	        props.put("group.id", "test-group");
+	         props.put("group.id", "test-group");
 	        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 	        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
