b62922b3999d12e01f75d717c89a96ccb37702b3
ganeshborde <52167545+ganeshborde@users.noreply.github.com>
2026-03-12 13:54:13 +0530

    git jenkin-test branch

--- src/main/java/com/example/taskmanagement/SimpleConsumer.java
+++ src/main/java/com/example/taskmanagement/SimpleConsumer.java
@@ -13,23 +13,9 @@ public class SimpleConsumer {
 	public static void main(String[] args) {
 		 Properties props = new Properties();
 	        props.put("bootstrap.servers", "localhost:9092");
-	        
-	        
-	        
-	        
 	         props.put("group.id", "test-group");
-	         
-	         
-	         
 	        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-	        
-	        
-	        
 	        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-	        
-	        
-	        
-	        
 
 	        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
 	        consumer.subscribe(Collections.singletonList("task-events"));
